f1d992294b9c950c2a2f5413e6a01321c8234c0c
Kunmun <kunmun.devroms@gmail.com>
2021-10-01 20:17:01 +0530

    UI: Revamp
    Change-Id: ab1e919cd5707e6dc729d9d51390b541cfab545a
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>
    Co-authored-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: RainySorcerer <beastdark704@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -5,6 +5,12 @@
 # global properties
 properties() { '
 kernel.string=Snowflake Kernel by RainZ
+kernel.for=
+kernel.compiler=
+kernel.made=
+kernel.version=
+message.word=
+build.date=$(date)
 do.devicecheck=1
 do.cleanup=1
 device.name1=violet
