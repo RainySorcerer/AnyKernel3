47118d560adeb73ad6d01da3f973c1df83428657
RainySorcerer <beastdark704@gmail.com>
2025-03-25 22:12:02 +0530

    AK3: Drop support for version older than A15
    Signed-off-by: RainySorcerer <beastdark704@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -19,7 +19,7 @@ device.name2=curtana
 device.name3=excalibur 
 device.name4=gram
 device.name5=joyeuse
-supported.versions=12.0-15.0
+supported.versions=15.0
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
