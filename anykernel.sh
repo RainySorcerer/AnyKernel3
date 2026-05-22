9170c2fed0ade245301cff19bfd712d88c8e3339
RainySorcerer <beastdark704@gmail.com>
2026-05-22 19:33:56 +0530

    AK3: Switch to Clang r596125 (22.0.2)
    Signed-off-by: RainySorcerer <beastdark704@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -5,7 +5,7 @@
 # global properties
 properties() { '
 kernel.string=Snowflake Kernel by RainZ
-kernel.compiler=Aosp Clang 20
+kernel.compiler=Aosp Clang 22.0.2
 kernel.made=RainZ
 message.word=Thx for installing Snowflake Kernel
 do.devicecheck=1
